0539c687e98c4789b9f54b942e324ddae947c73a
Nick Pierson <nkcrpn@gmail.com>
2015-07-12 11:32:04 -0500

    Refactor snake,apple,etc out of SnakeGame

--- snake.js
+++ snake.js
@@ -1,134 +1,148 @@
-function SnakeGame(canvas) {
-  var Direction = {
-    LEFT: 0,
-    RIGHT: 1,
-    UP: 2,
-    DOWN: 3
+var Direction = {
+  LEFT: 0,
+  RIGHT: 1,
+  UP: 2,
+  DOWN: 3
+}
+
+function Point(x, y) {
+  this.x = x;
+  this.y = y;
+}
+
+function ResetButton(canvas, game) {
+  this.x = 10;
+  this.y = canvas.height - 50;
+  this.width = 100;
+  this.height = 40;
+
+  var btn = this;
+
+  this.updateLocation = function() {
+    this.y = canvas.height - 50;
+  };
+
+  this.listen = function() {
+    window.addEventListener('mousedown', checkForClick, false);
+  };
+
+  function checkForClick(e) {
+    var eventX = e.pageX - canvas.offsetLeft;
+    var eventY = e.pageY - canvas.offsetTop;
+
+    var xCollides = btn.x <= eventX && eventX <= btn.x + btn.width;
+    var yCollides = btn.y <= eventY && eventY <= btn.y + btn.height;
+
+    if (xCollides && yCollides) {
+      game.reset();
+    }
   }
 
-  var context = canvas.getContext('2d');
+  this.draw = function(context) {
+    context.fillRect(this.x, this.y, this.width, this.height);
+
+    context.font = '16px sans-serif';
+    context.textAlign = 'center';
+    context.textBaseline = 'middle';
+    context.fillStyle = '#333';
+
+    var centerX = this.x + (this.width / 2);
+    var centerY = this.y + (this.height / 2);
+    context.fillText('Reset', centerX, centerY);
+  };
+}
+
+function Snake() {
+  // TODO snake should start near center of canvas
+  this.points = [new Point(50, 50)];
+  this.direction = Direction.RIGHT;
+
+  this.headCollidesWithWall = function(canvas) {
+    var head = this.points[0];
+    var xCollides = 0 > head.x || head.x > canvas.width - 10;
+    var yCollides = 0 > head.y || head.y > canvas.height - 10;
+    return xCollides || yCollides;
+  };
+
+  this.headCollidesWithSelf = function() {
+    var head = this.points[0];
+    for (i = 1; i < this.points.length; i++) {
+      if (head.x == this.points[i].x && head.y == this.points[i].y) {
+        return true;
+      }
+    }
+
+    return false;
+  };
+
+  this.headCollidesWithApple = function(apple){
+    var head = this.points[0];
+    return head.x === apple.x && head.y === apple.y;
+  };
+
+  this.turnLeft = function() {
+    if (this.direction !== Direction.RIGHT)
+      this.direction = Direction.LEFT;
+  };
+
+  this.turnRight = function() {
+    if (this.direction !== Direction.LEFT)
+      this.direction = Direction.RIGHT;
+  };
+
+  this.turnUp = function() {
+    if (this.direction !== Direction.DOWN)
+      this.direction = Direction.UP;
+  };
+
+  this.turnDown = function() {
+    if (this.direction !== Direction.UP)
+      this.direction = Direction.DOWN;
+  };
+
+  this.draw = function(context) {
+    context.fillStyle = 'black';
+
+    for (i = 0; i < this.points.length; i++) {
+      context.fillRect(this.points[i].x, this.points[i].y, 10, 10);
+    }
+  };
+}
+
+function Apple(canvas) {
+  this.x = generateX();
+  this.y = generateY();
+
+  this.move = function() {
+    this.x = generateX();
+    this.y = generateY();
+  };
+
+  function generateX() {
+    var x = Math.random() * (canvas.width - 10);
+    return Math.round(x / 10) * 10;
+  }
+
+  function generateY() {
+    var y = Math.random() * (canvas.height - 10);
+    return Math.round(y / 10) * 10;
+  }
+
+  this.draw = function(context) {
+    context.fillRect(this.x, this.y, 10, 10);
+  };
+}
+
+function SnakeGame(canvas) {
   var snake = new Snake();
   var apple = new Apple(canvas);
-  var btnReset = new ResetButton();
+  var btnReset = new ResetButton(canvas, this);
+
+  var context = canvas.getContext('2d');
 
   var time = Date.now();
   var gameOver = false;
   var score = 0;
-
-  function Point(x, y) {
-    this.x = x;
-    this.y = y;
-  }
-
-  function ResetButton() {
-    this.x = 10;
-    this.y = canvas.height - 50;
-    this.width = 100;
-    this.height = 40;
-
-    var btn = this;
-
-    this.updateLocation = function() {
-      this.y = canvas.height - 50;
-    }
-
-    function checkForCollision(e) {
-      var eventX = e.pageX - canvas.offsetLeft;
-      var eventY = e.pageY - canvas.offsetTop;
-
-      var xCollides = btn.x <= eventX && eventX <= btn.x + btn.width;
-      var yCollides = btn.y <= eventY && eventY <= btn.y + btn.height;
-
-      if (xCollides && yCollides) {
-        reset();
-      }
-    }
-
-    this.listen = function() {
-      window.addEventListener('mousedown', checkForCollision, false);
-    }
-
-    this.draw = function(context) {
-      context.fillRect(this.x, this.y, this.width, this.height);
-
-      context.font = '16px sans-serif';
-      context.textAlign = 'center';
-      context.textBaseline = 'middle';
-      context.fillStyle = '#333';
-
-      var centerX = this.x + (this.width / 2);
-      var centerY = this.y + (this.height / 2);
-      context.fillText('Reset', centerX, centerY);
-    }
-  }
-
-  function Snake() {
-    this.points = [new Point(50, 50)];
-    this.direction = Direction.RIGHT;
-
-    this.headCollidesWithWall = function(canvas) {
-      var head = this.points[0];
-      var xCollides = 0 > head.x || head.x > canvas.width - 10;
-      var yCollides = 0 > head.y || head.y > canvas.height - 10;
-      return xCollides || yCollides;
-    };
-
-    this.headCollidesWithSelf = function() {
-      var head = this.points[0];
-      for (i = 1; i < this.points.length; i++) {
-        if (head.x == this.points[i].x && head.y == this.points[i].y) {
-          return true;
-        }
-      }
-
-      return false;
-    }
-
-    this.headCollidesWithApple = function(apple){
-      var head = this.points[0];
-      return head.x === apple.x && head.y === apple.y;
-    };
-
-    this.turnLeft = function() {
-      if (this.direction !== Direction.RIGHT)
-        this.direction = Direction.LEFT;
-    };
-
-    this.turnRight = function() {
-      if (this.direction !== Direction.LEFT)
-        this.direction = Direction.RIGHT;
-    };
-
-    this.turnUp = function() {
-      if (this.direction !== Direction.DOWN)
-        this.direction = Direction.UP;
-    };
-
-    this.turnDown = function() {
-      if (this.direction !== Direction.UP)
-        this.direction = Direction.DOWN;
-    };
-  }
-
-  function Apple(canvas) {
-    this.x = generateX();
-    this.y = generateY();
-
-    this.move = function() {
-      this.x = generateX();
-      this.y = generateY();
-    }
-
-    function generateX() {
-      var x = Math.random() * (canvas.width - 10);
-      return Math.round(x / 10) * 10;
-    }
-
-    function generateY() {
-      var y = Math.random() * (canvas.height - 10);
-      return Math.round(y / 10) * 10;
-    }
-  }
 
   this.resize = function() {
     if (apple.x > canvas.width - 10 || apple.y > canvas.height - 10) {
@@ -137,17 +151,17 @@ function SnakeGame(canvas) {
 
     btnReset.updateLocation();
 
-    drawSnake(context, snake, apple);
-  }
+    drawGame();
+  };
 
-  function reset() {
+  this.reset = function() {
     gameOver = false;
     time = Date.now();
     score = 0;
 
     snake = new Snake();
     apple.move();
-  }
+  };
 
   this.play = function() {
     // listen for reset button
@@ -180,13 +194,13 @@ function SnakeGame(canvas) {
     // game loop
     function mainloop() {
       if (!gameOver) {
-        updateSnake(snake, apple);
-        drawSnake(context, snake, apple);
+        updateGame();
+        drawGame();
       }
     }
-  }
+  };
 
-  function updateSnake(snake, apple) {
+  function updateGame() {
     var newTime = Date.now();
 
     if (newTime - time > 50) {
@@ -224,18 +238,12 @@ function SnakeGame(canvas) {
     }
   }
 
-  function drawSnake(context, snake, apple) {
+  function drawGame() {
     context.clearRect(0, 0, canvas.width, canvas.height);
 
-    context.fillStyle = 'black';
+    snake.draw(context);
 
-    // draw snake
-    for (i = 0; i < snake.points.length; i++) {
-      context.fillRect(snake.points[i].x, snake.points[i].y, 10, 10);
-    }
-
-    // draw apple
-    context.fillRect(apple.x, apple.y, 10, 10);
+    apple.draw(context);
 
     // draw score
     context.font = 'bold 36px sans-serif';
@@ -244,7 +252,6 @@ function SnakeGame(canvas) {
     context.fillStyle = 'rgba(0, 0, 0, 0.1)';
     context.fillText('Score: ' + score, 10, 10);
 
-    // draw reset button
     btnReset.draw(context);
   }
 }
